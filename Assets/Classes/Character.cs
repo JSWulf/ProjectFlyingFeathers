f9a392274a85b181addf2088da218c121637f7ab
JWulf <JSWulf@users.noreply.github.com>
2021-05-18 17:09:14 -0700

    Movement Tweaks

--- Assets/Classes/Character.cs
+++ Assets/Classes/Character.cs
@@ -12,21 +12,27 @@ public class Character : MonoBehaviour
     private float xRotation = 0.0f;
     private float yRotation = 0.0f;
     private Camera cam;
+    //private Object Char;
+
+
     CharacterController characterController;
-    public float MovementSpeed = 1;
-    public float Gravity = 9.8f;
+    public float MovementSpeed = 12;
+    public float Gravity = 1f;
     private float velocity = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        //Char = this.gameObject;
         characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        print(Input.GetAxis("Horizontal"));
+        print(Input.GetAxis("Vertical"));
         float mouseX = Input.GetAxis("Mouse X") * horizontalSpeed;
         float mouseY = Input.GetAxis("Mouse Y") * verticalSpeed;
 
@@ -34,7 +40,8 @@ public class Character : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90, 90);
 
-        cam.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+        gameObject.transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+        
 
         // player movement - forward, backward, left, right
         float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
@@ -48,9 +55,11 @@ public class Character : MonoBehaviour
         }
         else
         {
-            velocity -= Gravity * Time.deltaTime;
+            velocity -= Gravity * MovementSpeed * Time.deltaTime;
             characterController.Move(new Vector3(0, velocity, 0));
         }
+
+        
     }
 }
 
