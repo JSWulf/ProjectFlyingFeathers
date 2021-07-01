d92c00fc0482f00320ace9d1746f32df79125281
iscroggs <iscroggs.is@gmail.com>
2021-06-30 18:04:41 -0600

    june

---
+++ Assets/Individuals/Joseph/TargetDestroy.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDestroy : MonoBehaviour
+{
+
+    //hit count and destroy for JSWLEVEL
+    /// <summary>
+    /// Set to 0 for no destroy
+    /// </summary>
+    public int MaxHit=5;
+    private int Hits = 0;
+
+    private List<GameObject> Arrows = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ArrowHit(TargetBehavior col, GameObject a)
+    {
+        if (MaxHit > 0)
+        {
+            Arrows.Add(a);
+
+            Hits++;
+            if (Hits >= MaxHit)
+            {
+                Destroy(gameObject);
+
+                foreach (var ar in Arrows)
+                {
+                    Destroy(ar);
+                }
+            }
+        }
+    }
+}
