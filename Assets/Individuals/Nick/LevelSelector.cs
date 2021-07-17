7999cedd8d217ed111a4133a5ec2e240a222123c
Nicholas S Anson <gizix@users.noreply.github.com>
2021-07-17 14:24:35 -0600

    Added Eric's Level button to Level Select
    Changed the behavior of the Level Select.  No longer dynamically creates panels/icons.  This can be added back in the ffuture when there are more levels.  For the time being, I think this will look better with the six that we have.

--- Assets/Individuals/Nick/LevelSelector.cs
+++ Assets/Individuals/Nick/LevelSelector.cs
@@ -25,16 +25,21 @@ public class LevelSelector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /*
         panelDimensions = levelHolder.GetComponent<RectTransform>().rect;
         iconDimensions = levelIcon1.GetComponent<RectTransform>().rect;
         int maxInARow = Mathf.FloorToInt((panelDimensions.width + iconSpacing.x) / (iconDimensions.width + iconSpacing.x));
         int maxInACol = Mathf.FloorToInt((panelDimensions.height + iconSpacing.y) / (iconDimensions.height + iconSpacing.y));
-        //amountPerPage = maxInARow * maxInACol;
-        //int totalPages = Mathf.CeilToInt((float)numberOfLevels / amountPerPage);
-        LoadPanels(2);
+        amountPerPage = maxInARow * maxInACol;
+        int totalPages = Mathf.CeilToInt((float)numberOfLevels / amountPerPage);
+        LoadPanels(1);
+        */
     }
+
+    /*
     void LoadPanels(int numberOfPanels)
     {
+        
         GameObject panelClone = Instantiate(levelHolder) as GameObject;
         PageSwiper swiper = levelHolder.AddComponent<PageSwiper>();
         swiper.totalPages = numberOfPanels;
@@ -47,8 +52,8 @@ public class LevelSelector : MonoBehaviour
             panel.name = "Page-" + (i+1);
             panel.GetComponent<RectTransform>().localPosition = new Vector2(panelDimensions.width * (i - 1), 0);
             SetUpGrid(panel);
-            //int numberOfIcons = i == numberOfPanels ? numberOfLevels - currentLevelCount : amountPerPage;
-            LoadIcons(3, panel);
+            int numberOfIcons = i == numberOfPanels ? numberOfLevels - currentLevelCount : amountPerPage;
+            LoadIcons(6, panel);
         }
         Destroy(panelClone);
     }
@@ -67,25 +72,22 @@ public class LevelSelector : MonoBehaviour
             switch (i)
             {
                 case 0:
-                    levelIcons[i] = levelIcon1;
+                    levelIcons[0] = levelIcon1;
                     break;
                 case 1:
-                    levelIcons[i] = levelIcon2;
+                    levelIcons[1] = levelIcon2;
                     break;
                 case 2:
-                    levelIcons[i] = levelIcon3;
+                    levelIcons[2] = levelIcon3;
                     break;
                 case 3:
-                    levelIcons[i] = levelIcon4;
+                    levelIcons[3] = levelIcon4;
                     break;
                 case 4:
-                    levelIcons[i] = levelIcon5;
+                    levelIcons[4] = levelIcon5;
                     break;
                 case 5:
-                    levelIcons[i] = levelIcon6;
-                    break;
-                default:
-                    levelIcons[i] = levelIcon1;
+                    levelIcons[5] = levelIcon6;
                     break;
             }
         }
@@ -99,6 +101,7 @@ public class LevelSelector : MonoBehaviour
             icon.name = "Level " + i;
         }
     }
+    */
 
     // Update is called once per frame
     void Update()
