d35e02a90fb030c1b5fbefb9d1cc1290b91dc0db
Nicholas S Anson <gizix@users.noreply.github.com>
2021-07-23 09:26:57 -0600

    Scale Level Select Canvas with screen size
    Should make the Level Select look proper

--- Assets/Individuals/Nick/SceneLoader.cs
+++ Assets/Individuals/Nick/SceneLoader.cs
@@ -10,15 +10,4 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(levelName);
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
